e7ae909ffeab2b5fba7a8ef5d69d7e27a45c9cb4
a449072124@qq.com <a44072124@qq.com>
2022-07-08 20:00:13 +0800

    20220708：陈慧超：图片上传前后端编写，后续读配置文件

--- back end/sport/src/main/java/com/hezong/MyConfig/WebConfig.java
+++ back end/sport/src/main/java/com/hezong/MyConfig/WebConfig.java
@@ -21,9 +21,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         //配置跨域
         registry.addMapping("/**")//添加允许跨域的路径
-                .allowedOrigins("http://localhost:80")  //前端哪些域名被允许跨域,也就是请求来源
+                .allowedOrigins("*")//前端哪些域名被允许跨域,也就是请求来源
                 .allowedMethods("GET","POST","PUT","OPTIONS","DELETE","HEAD","TRACE","CONNECT") //允许哪些请求方法跨域
                 .allowCredentials(true) //是否允许携带信息,例如token
+                .allowedHeaders("*")
                 .maxAge(3600);  //设置最大响应时间
     }
 }
